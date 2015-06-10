56c40702a2e0caf681debde59dac50236c7d9bdc
James Badger <james@jamesbadger.ca>
2015-06-10 15:51:23 -0600

    Disable developer, gitlab in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,11 +6,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :openid,
            store: OpenID::Store::Filesystem.new('tmp/store')
 
-  provider :developer unless Rails.env.production?
-  provider :gitlab, ENV['GITLAB_KEY'], ENV['GITLAB_SECRET'],
-    client_options: {
-      site: 'http://git.geocens.ca',
-      authorize_url: '/oauth/authorize',
-      token_url: '/oauth/token'
-    }
+  unless Rails.env.production?
+    provider :developer
+    provider :gitlab, ENV['GITLAB_KEY'], ENV['GITLAB_SECRET'],
+      client_options: {
+        site: 'http://git.geocens.ca',
+        authorize_url: '/oauth/authorize',
+        token_url: '/oauth/token'
+      }
+  end
 end
