5472d788a401ef1ddd8ccb2e240cc822ab4fb590
James Badger <james@jamesbadger.ca>
2015-06-10 16:55:51 -0600

    Convert port to integer for airbrake

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
     config.api_key = ENV['ERRBIT_KEY']
     config.host    = ENV['ERRBIT_HOST']
-    config.port    = ENV['ERRBIT_PORT']
+    config.port    = ENV['ERRBIT_PORT'].to_i
     config.secure  = config.port == 443
 end
