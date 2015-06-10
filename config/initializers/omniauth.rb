3766d07ebf539bc5415039fff236ee562ab59c30
James Badger <james@jamesbadger.ca>
2015-06-10 15:56:43 -0600

    Add twitter login support

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
   provider :openid,
            store: OpenID::Store::Filesystem.new('tmp/store')
+  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
 
   unless Rails.env.production?
     provider :developer
